ca7e82d33b1e07c2c876cc3e5d4e641eaf1c1929
eric-balisa <145489133+eric-balisa@users.noreply.github.com>
2023-10-18 16:05:53 -0300

    Add files via upload

---
+++ aula02/exp01.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="exp01.css">
+</head>
+<body>
+    <pre> Carinhanha</pre>
+    <h1> titulo 1</h1>
+    <h2>subtitulo</h2>
+    <p> paragrafo</p>
+    <p class="paragrafo2"></p>
+    <p id="paragrafo1"> carinhanha a melhor da bahia </p>
+    <?php
+
+
+?>
+</body>
+</html>
